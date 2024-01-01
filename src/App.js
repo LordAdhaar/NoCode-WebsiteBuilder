bbf72bf2161fa3fc074b1a03abec21c59269ef9b
LordAdhaar <jainadhaar2002@gmail.com>
2024-01-01 20:28:11 +0530

    add save button to log HTML and CSS in the console and also download as zip file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from "react";
 import grapesjs from "grapesjs";
 import "./styles/main.scss";
-import gjsPresetWebpage from "grapesjs-preset-webpage";
 import gjsBasicBlocks from "grapesjs-blocks-basic";
+import grapesjsExportPlugin from "grapesjs-plugin-export"
 
 function App() {
 
@@ -11,20 +11,39 @@ function App() {
   
     const editor = grapesjs.init({
       container : "#editor",
-      plugins: [gjsPresetWebpage, gjsBasicBlocks] ,
+      plugins: [ gjsBasicBlocks, grapesjsExportPlugin] ,
       pluginOpts:{
-        gjsPresetWebpage:{},
         gjsBasicBlocks:{},
-      },
+      }, 
+
     });
+
+    editor.Panels.addPanel({
+      el: '.gjs-pn-options',
+      buttons: [
+        {
+          id: 'save-button',
+          className: 'btn-save-button',
+          label: 'Save',
+          command(editor) { 
+            console.log(editor.getHtml());
+            console.log(editor.getCss());
+            editor.runCommand("gjs-export-zip");
+          }
+        }
+      ]
+    });
+
     setEditor(editor);
+
+    
 
   }, [])
   
 
   return (
     <div className="App">
-      <div id="editor"></div>
+        <div id="editor"></div>  
     </div>
   );
 }
